1f0956bcba1108a0bed006abaad3efb0e22834b8
Matthew Printz <matt@jataware.com>
2025-01-31 14:10:25 -0600

    Update setup.py file to reflect changes in upstream palimpzest

--- src/bdf_pz/procedures/python3/setup.py
+++ src/bdf_pz/procedures/python3/setup.py
@@ -1,4 +1,5 @@
 import palimpzest as pz
+from palimpzest.core import PDFFile
 import pandas as pd
 import time
 import os
@@ -8,26 +9,26 @@ formatter = IPython.get_ipython().display_formatter.formatters['text/plain']
 formatter.max_seq_length = 0
 
 # set OPENAI_API_KEY environment variable based on OPENAI_API_KEY
-os.environ["OPENAI_API_KEY"] = "{{ OPENAI_API_KEY }}" 
+os.environ["OPENAI_API_KEY"] = "{{ OPENAI_API_KEY }}"
 
-class ScientificPaper(pz.PDFFile):
+class ScientificPaper(pz.core.PDFFile):
    """Represents a scientific research paper, which in practice is usually from a PDF file"""
-   paper_title = pz.Field(desc="The title of the paper. This is a natural language title, not a number or letter.", required=True)
-   author = pz.Field(desc="The name of the first author of the paper", required=True)
-   abstract = pz.Field(desc="A short description of the paper contributions and findings", required=False)
+   paper_title = pz.core.Field(desc="The title of the paper. This is a natural language title, not a number or letter.")
+   author = pz.core.Field(desc="The name of the first author of the paper")
+   abstract = pz.core.Field(desc="A short description of the paper contributions and findings")
 
-class Reference(pz.Schema):
+class Reference(pz.core.Schema):
     """ Represents a reference to another paper, which is cited in a scientific paper"""
-    index = pz.Field(desc="The index of the reference in the paper", required=True)
-    title = pz.Field(desc="The title of the paper being cited", required=True)
-    first_author = pz.Field(desc="The author of the paper being cited", required=True)
-    year = pz.Field(desc="The year in which the cited paper was published", required=True)
+    index = pz.core.Field(desc="The index of the reference in the paper")
+    title = pz.core.Field(desc="The title of the paper being cited")
+    first_author = pz.core.Field(desc="The author of the paper being cited")
+    year = pz.core.Field(desc="The year in which the cited paper was published")
 
 import IPython
 formatter = IPython.get_ipython().display_formatter.formatters['text/plain']
 formatter.max_seq_length = 0
 
-from palimpzest.corelib.schemas import File, Number, TextFile, RawJSONObject, PDFFile, ImageFile, EquationImage, PlotImage, URL, Download, WebPage, XLSFile, Table
+from palimpzest.core import File, Number, TextFile, RawJSONObject, PDFFile, ImageFile, EquationImage, PlotImage, URL, Download, WebPage, XLSFile, Table
 
 existing_schemas = {
     "File": File,
