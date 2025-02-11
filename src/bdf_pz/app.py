2efea477edb65668c99a2435051dd02c6655cd58
Matthew Printz <matt@jataware.com>
2025-02-11 14:58:16 -0600

    Changes to reflect updates to beaker-kernel app definition

--- src/bdf_pz/app.py
+++ src/bdf_pz/app.py
@@ -1,49 +1,53 @@
-import json
-import os
 
-from beaker_kernel.lib.app import BeakerApp, BeakerAppAsset, AppConfigStrings, Context
+from beaker_kernel.lib.app import BeakerApp
 
 
 class PalimpzestApp(BeakerApp):
-    SLUG = "palimpzest"
-    ASSET_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "assets"))
+    slug = "palimpzest"
+    name = "PalimpChat"
 
     pages = {
-        'chat': {
-            "title": "Palimpzest chat",
+        "chat": {
+            "title": "{app_name}",
             "default": True,
         },
         "notebook": {
             "title": "Palimpzest notebook",
         },
         "dev": {
-            "title": "Dev interface",
+            "title": "Palimpzest dev interface",
         }
     }
-    default_context = Context(
-        slug="bdf-pz",
-        payload={},
-    )
-    single_context = True
 
-    assets = [
-        BeakerAppAsset(
-            slug="header_logo",
-            src="palimpzest-cropped.png",
-            alt="Palimpzest logo"
-        ),
-        BeakerAppAsset(
-            slug="body_logo",
-            src="palimpzest-cropped.png",
-            alt="Palimpzest cropped logo"
-        )
-    ]
-    strings = AppConfigStrings(
-        short_title="Palimpzest",
-        chat_welcome_html="""<div style="display: flex; flex-direction: row; align-items: center; gap: 20px;">
-          <img src="/assets/palimpzest/palimpzest-cropped.png" alt="Palimpzest Logo" height="75px">
+    stylesheet = "global_stylesheet"
+
+    default_context = {
+        "slug": "bdf-pz",
+        "payload": {},
+        "single_context": True,
+    }
+
+    assets = {
+        "header_logo": {
+            "src": "palimpzest-cropped.png",
+            "alt": "{app_name} logo",
+        },
+        "body_logo": {
+            "src": "palimpzest-cropped.png",
+            "alt": "{app_name} cropped logo",
+            "height": "75px"
+        },
+        "global_stylesheet": {
+            "src": "style.css",
+        }
+    }
+
+    template_bundle = {
+        "short_title": "{app_name}",
+        "chat_welcome_html": """<div style="display: flex; flex-direction: row; align-items: center; gap: 20px;">
+          <img src="{asset:body_logo:src}" alt="{asset:body_logo:alt}" height="{asset:body_logo:height}">
           <p>Hi! I'm your Palimpzest Agent and I can help you do all sorts of information extraction tasks. Your first step is to upload
           some documents or datasets to get started. Then let me know what kind of information you'd like to extract from them.
           Let me know what you'd like to do so I can best assist you!</p>
-        </div>"""
-    )
+        </div>""",
+    }
